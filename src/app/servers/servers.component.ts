741f6d1b38d7b6634390690398657628cd7f77ed
Kalon Ohmstede <kalonohmstede@gmail.com>
2022-09-22 12:43:10 -0500

    another homework assignment

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -14,6 +14,7 @@ export class ServersComponent implements OnInit {
   serverCreationStatus = 'No server was created.'
   serverName = 'Testserver';
   serverCreated = false;
+  servers = ['Testserver', 'Testserver 2']
 
   constructor() {
     setTimeout(() => {
@@ -25,8 +26,10 @@ export class ServersComponent implements OnInit {
   }
 
   onCreateServer() {
-    this.serverCreationStatus = 'Server was created. Name is ' + this.serverName;
     this.serverCreated = true;
+    this.servers.push(this.serverName);
+    this.serverCreationStatus = 'Server was created. Name is ' + this.serverName;
+
   }
 
   onUpdateServerName(event: any) {
